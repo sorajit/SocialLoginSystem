acc2bb0ed7477594491464a54edb6e968d0f3d8a
sorajit <sorajit.a@gmail.com>
2024-05-06 06:25:18 +0700

    edit secret

--- passport/lib/fbpassport.js
+++ passport/lib/fbpassport.js
@@ -3,8 +3,8 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 require('dotenv').config();
 
 passport.use(new FacebookStrategy({
-  clientID: '975082626986967',
-  clientSecret: '1261cb7f84be51c1f1b251c36c3d3eb7',
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_SECRET_ID,
   callbackURL: 'http://localhost:3000/auth/facebook/callback',
   state: true
   },
